df9e4f8fbbeee703dbcecc22c617b444e679b377
Florentin Portets <florentin.portets@ynov.com>
2026-01-27 13:38:58 +0100

    feat: initialize React project with TypeScript, Vite, and Tailwind CSS

---
+++ cinelink-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="min-h-screen bg-black flex items-center justify-center">
+            <p className="text-red-500 text-3xl font-bold">
+                CineLink Frontend
+            </p>
+        </div>
+    );
+}
+
+export default App;
